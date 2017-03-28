876b84af680dad5b95354d67fe320a09f0233d97
Hayden Wagner (Student Employee) <tlshbw@ad.rit.edu>
2017-03-28 19:25:17 -0400

    Specific tls-asana lib work...may not need socket for CSV button click check todo in server.js

--- js/main.js
+++ js/main.js
@@ -11,3 +11,36 @@ var socket = io.connect(originURL, {
 socket.on('connected', function(data){
     console.log(data);
 })
+
+socket.on('getTaskCSVStuffSuccess', function(res){
+    console.log(res);
+});
+
+socket.on('getTaskCSVStuffFailure', function(err,res){
+    console.log(err);
+});
+
+
+
+//------------------
+
+$(function(){
+    var tempTaskArr = [1,2,3,4,5];
+    var container = $('#main');
+
+    tempTaskArr.forEach(function(x){
+        var card = `<div class="card tlsTask float-left" style="width: 20rem;">
+                        <div class="card-block">
+                            <h4 class="card-title">Task `+ x +`</h4>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary tlsTaskBtn" tlstaskdata="`+ x +`">Go somewhere</a>
+                        </div>
+                    </div>`
+
+        container.append( card );
+    })
+
+    $('.tlsTaskBtn').on('click', function(){
+        socket.emit('request-task', {'id': this.getAttribute('tlstaskdata')})
+    })
+}());
--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const tlsAsana = require('tls-asana');
 
-const ASANA_PROJECT_ID = '245703302055184';
+const ASANA_PROJECT_ID = '196673359742158';
 
 let app = express();
 
@@ -25,6 +25,26 @@ io.on('connection', function(socket){
     socket.emit('connected', "Backend connection response");
 
     socket.on('request-task', (testParam) => {
-        console.log('Client to server success: ' + testParam);
+        console.log('Client to server success: ' + testParam.id);
+
+        //this will be a real task id in future, just a test one right now
+        var testTaskID = 196674113316972;
+
+        //TODO
+        //may not actually need this....may not need a method in the tlsAsana lib that has to do anything 
+        //with the CSV request...we may just be able to have all the info we need contained in the task
+        //object tags (which we will need to request from the tlsAsana lib anyways and we don't need to wait
+        //for a user to click and send data back and forth through the socket). We should just be able to have
+        //a frontend function that runs that looks at the task object, makes a CSV, and then handles the CSV download
+        //like we did with the color science app
+
+        //socket can establish connection and send tasks originally, but CSV download can all be handled on frontend
+        tlsAsana.getTaskCSVStuff( testTaskID ).then(res=>{
+            console.log(res);
+            socket.emit('getTaskCSVStuffSuccess', res);
+        }).catch(err => {
+            console.log(res);
+            socket.emit('getTaskCSVStuffFailure', err, res);
+        });
     })
 });
